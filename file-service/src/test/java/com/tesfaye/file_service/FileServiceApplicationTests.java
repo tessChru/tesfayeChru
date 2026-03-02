e4dd74da2d8e200647118e5b74150344647536bf
Tesfaye Chru <tess.chru@gmail.com>
2026-03-02 09:59:39 -0600

    initial project structure with docker-compose

---
+++ file-service/src/test/java/com/tesfaye/file_service/FileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesfaye.file_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
